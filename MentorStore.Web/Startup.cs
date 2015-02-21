5d55df2a2086ef534ed3a991351e873e0d267515
Maks Martynov <maks.martynv@gmail.com>
2015-02-21 17:22:39 +0200

    Initial

---
+++ MentorStore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MentorStore.Web.Startup))]
+namespace MentorStore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

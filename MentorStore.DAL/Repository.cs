5072fb8b6f5c3651134c646728a851ca862c7a7a
monokarp <mnkrp.ml@gmail.com>
2015-02-28 10:42:38 +0200

    Somehow missed out on the repository class

---
+++ MentorStore.DAL/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace MentorStore.DAL
+{
+    class Repository<C, T> : IRepository<T>
+        where T : class
+        where C : DbContext, new()
+    {
+    }
+}

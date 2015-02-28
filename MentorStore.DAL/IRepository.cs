2db7d5abf7b32d4e9d12dbd22895147affd7ded0
monokarp <mnkrp.ml@gmail.com>
2015-02-28 10:37:14 +0200

    Added EF references

--- MentorStore.DAL/IRepository.cs
+++ MentorStore.DAL/IRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MentorStore.DAL
+﻿namespace MentorStore.DAL
 {
     interface IRepository<TEntity>
         where TEntity : class

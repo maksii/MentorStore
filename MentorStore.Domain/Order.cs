c9ae8bc8e1289070f3fe7e32a5ce8ecf0b95242e
youRooock <junioriks@rambler.ru>
2015-02-28 14:11:56 +0200

    Added 3 classes

---
+++ MentorStore.Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorStore.Domain
+{
+    public class Order : BaseEntity
+    {
+        public string TrackingNumber { get; set; }
+        public string Address { get; set; }
+        public string CustomerName { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}

a5f27a7c152e8dc1adbecad65d3b385bdff76323
monokarp <mnkrp.ml@gmail.com>
2015-03-01 22:09:58 +0200

    Generic EF context upd

---
+++ MentorStore.DAL.DB/IContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MentorStore.DAL.DB
+{
+    interface IContext : IDisposable
+    {
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+        int SaveChanges();
+    }
+}

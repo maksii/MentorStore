b3cfea01a7468e983fc1a041c3ac8baae754b91c
tarasmychats <tarasmychats@gmail.com>
2015-03-03 00:43:01 +0200

    edited Layout

--- MentorStore.Web/App_Start/BundleConfig.cs
+++ MentorStore.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace MentorStore.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = false;
         }
     }
 }

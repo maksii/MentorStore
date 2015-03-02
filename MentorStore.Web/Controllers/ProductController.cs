370585872f1969ac461ad566ef9c2cf853396852
youRooock <junioriks@rambler.ru>
2015-03-02 23:28:47 +0200

    Implemented DI for OrderController, CustomerController, ProductController.

--- MentorStore.Web/Controllers/ProductController.cs
+++ MentorStore.Web/Controllers/ProductController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MentorStore.BL;
+using MentorStore.Domain;
 
 namespace MentorStore.Web.Controllers
 {
     public class ProductController : Controller
     {
+        private IMainLogic<Product> _mainLogic;
+
+        public ProductController(IMainLogic<Product> mainLogic)
+        {
+            this._mainLogic = mainLogic;
+        }
+
         public ActionResult Index()
         {
             return View();
